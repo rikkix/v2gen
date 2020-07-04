a40d731ced66b347e0ed20257979e0fc46847722
Richard Chen <i@iochen.com>
2020-07-04 22:19:43 +0800

    fix #17

--- cmd/v2gen/main.go
+++ cmd/v2gen/main.go
@@ -1,9 +1,9 @@
-package v2gen
+package main
 
 import (
 	"flag"
 	"fmt"
-	v2gen2 "iochen.com/v2gen"
+	"iochen.com/v2gen"
 	"iochen.com/v2gen/infra/miniv2ray"
 )
 
@@ -14,14 +14,14 @@ var (
 func main() {
 	flag.Parse()
 
-	if *v2gen2.FlagVersion {
+	if *v2gen.FlagVersion {
 		fmt.Println("v2gen version:", Version)
 		fmt.Println("V2Ray Core version:", miniv2ray.CoreVersion())
 		return
 	}
 
-	if *v2gen2.FlagInit {
-		if err := v2gen2.InitV2GenConf(*v2gen2.FlagUserConf); err != nil {
+	if *v2gen.FlagInit {
+		if err := v2gen.InitV2GenConf(*v2gen.FlagUserConf); err != nil {
 			panic(err)
 		}
 		fmt.Println("v2gen config initialized")
@@ -30,10 +30,10 @@ func main() {
 
 	var err error
 
-	if len(*v2gen2.FlagURIs) != 0 {
-		err = v2gen2.GenFromSecRawData(*v2gen2.FlagURIs)
-	} else if *v2gen2.FlagURL != "" {
-		err = v2gen2.GenFromURL(*v2gen2.FlagURL)
+	if len(*v2gen.FlagURIs) != 0 {
+		err = v2gen.GenFromSecRawData(*v2gen.FlagURIs)
+	} else if *v2gen.FlagURL != "" {
+		err = v2gen.GenFromURL(*v2gen.FlagURL)
 	} else {
 		panic("nothing to do")
 	}
